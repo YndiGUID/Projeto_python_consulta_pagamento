84779fafc9a2ed54272a5e6ecfca0f73d6083d4f
IngridDias <diasingrid56@gmail.com>
2024-10-17 08:17:34 -0300

    ''' 1. entrar na plhanilha e extrair o CPF do cliente 2. entrar no site ______________________ e use o CPF da planilha para pesquisar o status de pagamento da quele cliente 3. verificar se esta "em dia" ou "em atraso" 4. se estiver "em dia" pegar a data de pagamento e o metodo de pagamento 5. caso contrario, colocar status "pendente"
    ''''

---
+++ app.py
@@ -0,0 +1,36 @@
+import openpyxl
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
+from time import sleep
+
+Planilha_Clientes = openpyxl.load_workbook('dados_clientes.xlsx')
+pagina_clientes = Planilha_Clientes['Sheet1']
+
+
+for linha in pagina_clientes.iter_rows(min_row=2,values_only=True):
+   Nome, Valor,CPF,Vencimento = linha
+
+   driver = webdriver.Chrome()
+   driver.get('https://consultcpf-devaprender.netlify.app/')
+   sleep(5)
+   campo_pesquisa = webdriver.find_element(By.XPATH,"//input[@id='cpfInput']")
+   sleep(1)
+   campo_pesquisa.send_keys(CPF)
+   sleep(1)
+   botao_pesquisar = driver.find_element(By.XPATH,"//button[@class='btn btn-custom btn-lg btn-block mt-3']")
+   sleep(1)
+   botao_pesquisar.click()
+   sleep(4)
+
+   status = driver.find_element(By.XPATH,"//span[@id='statusLabel']")
+   if status.text == 'em dia':
+     data_pagamento = driver.find_element(By.XPATH,"//p[@id='paymentDate']")
+     metodo_pagamento = driver.find_element(By.XPATH,"//p[@id='paymentMethod']")
+     data_pagamento_limpo = data_pagamento.text.split()[3]
+      
+     pagina_fechamento.append([Nome,Valor,CPF,Vencimento,'em dia','xxxx-xx-xx','xxxx'])
+   else:
+      planilha_fechamento = openpyxl.load_workbook('planilha_fechamento.xlsx')
+      pagina_fechamento = planilha_fechamento['Sheet1']
+
+      pagina_fechamento.append([Nome,Valor,CPF,Vencimento,'Pendente'])
