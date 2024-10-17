4b02f1be15bb32117fcb34d32f1a3b72dde14474
Ingrid Santos <113631752+YndiGUID@users.noreply.github.com>
2024-10-17 11:08:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,9 @@ for linha in pagina_clientes.iter_rows(min_row=2,values_only=True):
    driver = webdriver.Chrome()
    driver.get('https://consultcpf-devaprender.netlify.app/')
    sleep(5)
-   campo_pesquisa = webdriver.find_element(By.XPATH,"//input[@id='cpfInput']")
+   campo_pesquisa = driver.find_element(By.XPATH,"//input[@id='cpfInput']")
    sleep(1)
+   campo_pesquisa.clear()
    campo_pesquisa.send_keys(CPF)
    sleep(1)
    botao_pesquisar = driver.find_element(By.XPATH,"//button[@class='btn btn-custom btn-lg btn-block mt-3']")
@@ -26,11 +27,19 @@ for linha in pagina_clientes.iter_rows(min_row=2,values_only=True):
    if status.text == 'em dia':
      data_pagamento = driver.find_element(By.XPATH,"//p[@id='paymentDate']")
      metodo_pagamento = driver.find_element(By.XPATH,"//p[@id='paymentMethod']")
+     
      data_pagamento_limpo = data_pagamento.text.split()[3]
+     metodo_pagamento_limpo = metodo_pagamento.text.split()[3]
       
-     pagina_fechamento.append([Nome,Valor,CPF,Vencimento,'em dia','xxxx-xx-xx','xxxx'])
+     planilha_fechamento = openpyxl.load_workbook('planilha_fechamento.xlsx')
+     pagina_fechamento = planilha_fechamento['Sheet1']
+      
+     pagina_fechamento.append([Nome,Valor,CPF,Vencimento,'em dia',data_pagamento_limpo,metodo_pagamento_limpo])
+     planilha_fechamento.save('planilha_fechamento.xlsx')
+     
    else:
       planilha_fechamento = openpyxl.load_workbook('planilha_fechamento.xlsx')
       pagina_fechamento = planilha_fechamento['Sheet1']
 
       pagina_fechamento.append([Nome,Valor,CPF,Vencimento,'Pendente'])
+      planilha_fechamento.save('planilha_fechamento.xlsx')
